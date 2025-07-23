20aba44d6fed7bfe015a06f6753c76b52d35a28c
RinTheCat <liza@danilova.me>
2025-07-23 19:18:21 +0300

    Задача на RateLimiter: исправление и рефакторинг

--- src/main/java/ru/tasks/demo/task1/RateLimiterImpl.java
+++ src/main/java/ru/tasks/demo/task1/RateLimiterImpl.java
@@ -5,28 +5,38 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class RateLimiterImpl implements RateLimiter {
 
-    private final Map<String, double[]> tasks = new ConcurrentHashMap<>();
+    private final Map<String, RateLimiterTask> tasks = new ConcurrentHashMap<>();
+
+    private static class RateLimiterTask {
+        private final double permitsPerSec;
+        private long lastProcessedTime;
+
+        public RateLimiterTask(double permitsPerSec) {
+            this.permitsPerSec = permitsPerSec;
+            this.lastProcessedTime = System.currentTimeMillis();
+        }
+    }
 
     @Override
     public boolean createNewTask(String task1, double permitsPerSec) {
         if (permitsPerSec <= 0) return false;
-        tasks.put(task1, new double[] {permitsPerSec, 0});
+        tasks.put(task1, new RateLimiterTask(permitsPerSec));
         return true;
     }
 
     @Override
     public boolean register(String taskId) {
         final long currentTime = System.currentTimeMillis();
-        final double[] result = tasks.computeIfPresent(taskId, (k, v) -> {
-            double permitsPerSec = v[0];
-            double lastProcessed = v[1];
-            if (currentTime - lastProcessed > permitsPerSec * 1000) {
-                return new double[] {permitsPerSec, currentTime};
-            } else {
-                System.out.println("RateLimiter exceeded");
-                return v;
+        final RateLimiterTask task = tasks.get(taskId);
+        if (task == null) return false;
+
+        synchronized (task) {
+            if (currentTime - task.lastProcessedTime > 1000L / task.permitsPerSec) {
+                task.lastProcessedTime = currentTime;
+                return true;
             }
-        });
-        return result != null && result[1] != currentTime;
+        }
+        System.out.println("RateLimiter exceeded");
+        return false;
     }
 }
